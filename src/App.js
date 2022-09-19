1e5282f4d102ca66aeb78c7fabf6baa0d6a8c761
TankDodi16 <tankdodi@gmail.com>
2022-09-19 14:07:51 +0530

    image-file-resizer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Resizer from "react-image-file-resizer";
+import React, { useState } from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [data, setData] = useState("");
+	const compress = 80;
+	const fileFormat = "JPEG";
+	const rotationDegree = 0; // degree of clockwise rotation to apply to uploaded image.
+	const resizeFile = (file, height, width) =>
+		new Promise((resolve) => {
+			Resizer.imageFileResizer(
+				file,
+				width,
+				height,
+				fileFormat,
+				compress,
+				rotationDegree,
+				(uri) => {
+					resolve(uri);
+				},
+				"base64"
+			);
+		});
+
+	const onChange = async (event) => {
+		try {
+			let _URL = window.URL || window.webkitURL;
+			let img = new Image();
+			const file = event.target.files[0];
+			let height, width;
+
+			let objectUrl = _URL.createObjectURL(file);
+			img.onload = function () {
+				height = this.height;
+				width = this.width;
+				_URL.revokeObjectURL(objectUrl);
+			};
+			console.log("file", file);
+			const image = await resizeFile(file, height, width);
+			img.src = objectUrl;
+
+			console.log(image);
+			setData(image);
+		} catch (err) {
+			console.log(err);
+		}
+	};
+	return (
+		<>
+			<div>
+				<input type="file" onChange={onChange} />
+				<img src={data} />
+			</div>
+		</>
+	);
 }
 
 export default App;
